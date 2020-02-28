74a6bb159d8e30f3daea94e800795bdddfe11655
Sven Fiergolla <sven.fiergolla@gmail.com>
2020-02-28 15:01:33 +0100

    Added benchmark scripts and results for f2fs on a 32G flash drive

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,10 +4,12 @@ set -e
 
 echo "Starting benchmark with fio on /dev/sdd1 ..."
 
-fio --name=guestrun --filename=/dev/sdd1 \
- --rw=randwrite --bs=16k --ioengine=libaio --direct=1 \
- --norandommap --runtime=120 --time_based --numjobs=1 \
- --group_reporting --thread --size=25g --write_lat_log \
- --write_bw_log --iodepth=74
+echo "Starting with sequential write and read."
+
+fio fio-seq-RW.fio |& tee sequential-result.txt
+
+echo "Starting with random write and read."
+
+fio fio-rand-RW.fio |& tee random-result.txt
 
 echo "Done."
